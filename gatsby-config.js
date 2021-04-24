c98e2ae59770691ecd994821ede53680f7669919
Catherine Pollock <pollockcr@gmail.com>
2021-04-24 17:43:21 -0400

    add tailwind

--- gatsby-config.js
+++ gatsby-config.js
@@ -110,6 +110,15 @@ module.exports = {
       },
     },
     {
+      resolve: `gatsby-plugin-sass`,
+      options: {
+        postCssPlugins: [
+          require("tailwindcss"),
+          require("./tailwind.config.js"), // Optional: Load custom Tailwind CSS configuration
+        ],
+      },
+    },
+    {
       resolve: `gatsby-plugin-manifest`,
       options: {
         name: `Gatsby Starter Blog`,
