783b4129d47d78997d1acc601bbd3597a38d0470
Catherine Pollock <pollockcr@gmail.com>
2021-05-19 15:37:15 -0600

    remove icon

--- gatsby-config.js
+++ gatsby-config.js
@@ -121,13 +121,13 @@ module.exports = {
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: `Gatsby Starter Blog`,
-        short_name: `GatsbyJS`,
+        name: `Matthew Grube`,
+        short_name: `Matthew Grube`,
         start_url: `/`,
         background_color: `#ffffff`,
         theme_color: `#663399`,
         display: `minimal-ui`,
-        icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
+        icon: ``, // This path is relative to the root of the site.
       },
     },
     `gatsby-plugin-react-helmet`,
